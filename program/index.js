a767fc21b54f03bfe5935fb14f6f700bf1d15a3f
Bernardus Yoga <bernarduuuus@gmail.com>
2025-12-18 14:44:22 +0700

    add query.sql answer

---
+++ program/index.js
@@ -0,0 +1,16 @@
+function Generator(input) {
+    if (input <= 0) {
+        return -1
+    }
+
+    let strInput = String(input)
+    let arrInput = strInput.split("")
+    let result = 0
+    for (let i = 0; i < arrInput.length;i++) {
+        result += Number(arrInput[i])
+    }
+    result += input
+    return result
+}
+
+console.log(Generator(805));
